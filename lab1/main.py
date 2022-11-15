74b81f93f553fa99837b9d4fa6968b4340a79aef
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-11-16 00:55:02 +0500

    finish lab 1

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,6 @@
 import random
 import statistics
 import math
-from gmpy2 import mpz
 
 
 def series_sum():
@@ -20,7 +19,12 @@ def median_product_arr():
     for i in range(0, 1000000):
         array.append(random.randint(1, 10))
     print(statistics.median(array))
-    print(mpz(math.prod(array)))
+    n = math.prod(array)
+    count = 0
+    while n % 10 == 0:
+        count += 1
+        n //= 10
+    print(n, '*10^', count)
 
 
 if __name__ == '__main__':
