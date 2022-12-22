7075ddf00a1ff20318c6c974105c92efd4e0162b
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-12-22 12:41:24 +0500

    finish

--- lab3/main.py
+++ lab3/main.py
@@ -8,6 +8,6 @@ pyDatalog.create_terms('factorial, N')
 factorial[N] = N * factorial[N - 1]
 factorial[1] = 1
 
-print((Sum_n == summ)&(Avg==Sum_n/1000000)&(Median==median))
+print((Sum_n == summ)&(Avg == Sum_n/1000000)&(Median == median))
 print()
-print(Prod_n==factorial[100])
+print(Prod_n == factorial[100])
