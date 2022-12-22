b3ee8529c09829a093b4018207386b3d95d6c848
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-12-22 12:05:04 +0500

    add summ

---
+++ lab3/main.py
@@ -0,0 +1,13 @@
+from pyDatalog import pyDatalog
+
+summ = ((0+999999)*1000000)/2
+avg = summ / 1000000
+median = 100 / 2
+
+pyDatalog.create_terms('summ, amount, n, ,avg,N3')
+
+Factorial[n]=n*Factorial[n-1]
+Factorial[1]=1
+
+print(summ==summ)
+
