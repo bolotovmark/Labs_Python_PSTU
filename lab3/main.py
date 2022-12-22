eb28ef80bf98e2fdd2e4654fe737a350d7e792fa
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-12-22 12:40:57 +0500

    add median and prod_n

--- lab3/main.py
+++ lab3/main.py
@@ -1,13 +1,13 @@
 from pyDatalog import pyDatalog
 
-summ = ((0+999999)*1000000)/2
-avg = summ / 1000000
+summ = ((0 + 999999) * 1000000) / 2
 median = 100 / 2
 
-pyDatalog.create_terms('summ, amount, n, ,avg,N3')
+pyDatalog.create_terms('Sum_n, Avg, Median, Prod_n')
+pyDatalog.create_terms('factorial, N')
+factorial[N] = N * factorial[N - 1]
+factorial[1] = 1
 
-Factorial[n]=n*Factorial[n-1]
-Factorial[1]=1
-
-print(summ==summ)
-
+print((Sum_n == summ)&(Avg==Sum_n/1000000)&(Median==median))
+print()
+print(Prod_n==factorial[100])
